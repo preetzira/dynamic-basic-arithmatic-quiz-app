cbb7594d973b71757aa42b638b5c970a14bbf4b7
Gurpreet Singh <preetzira08@gmail.com>
2020-04-03 13:37:09 +0530

    updated code routes and registered service workers

--- src/App.js
+++ src/App.js
@@ -11,25 +11,25 @@ import Quiz from './components/Quiz'
 import Results from './components/Results'
 import NotFound from './components/NotFound'
 
-const Iframe = (props) =>{
-  return <div className="container-fluid">
-    <div className="row">
-      <div className="col-md-6">
-        <iframe src="/index" title="quizOne" frameBorder="0" width="100%" height="650px"></iframe>
-      </div>
-      <div className="col-md-6">
-        <iframe src="/index" title="quizTwo" frameBorder="0" width="100%" height="650px"></iframe>
-      </div>
-    </div>
-  </div>
-}
+// const Iframe = (props) =>{
+//   return <div className="container-fluid">
+//     <div className="row">
+//       <div className="col-md-6">
+//         <iframe src="/index" title="quizOne" frameBorder="0" width="100%" height="650px"></iframe>
+//       </div>
+//       <div className="col-md-6">
+//         <iframe src="/index" title="quizTwo" frameBorder="0" width="100%" height="650px"></iframe>
+//       </div>
+//     </div>
+//   </div>
+// }
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Iframe} />
-        <Route path="/index" component={Index} />
+        {/* <Route exact path="/" component={Iframe} /> */}
+        <Route path="/" component={Index} />
         <Route path="/question/:id" component={Quiz} />
         <Route path="/results" component={Results} />
         <Route component={NotFound} />
