30bc8da8ea1b1f9c0d8bc55090f106c5c34e111d
Gurpreet Singh <preetzira08@gmail.com>
2020-09-09 22:46:28 +0530

    debugging on production

--- public/react-dev-tools-hider.js
+++ public/react-dev-tools-hider.js
@@ -1,3 +1,1 @@
-if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
-    window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () { };
-}
+window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = function () { }
